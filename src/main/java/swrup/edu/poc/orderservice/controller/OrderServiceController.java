9fcfb8b9a124943300084bd3dfb0a869804abe8e
Swarup Dutta <swarupcmsadutta@gmail.com>
2021-03-17 14:50:59 +0530

    Commit to check

--- src/main/java/swrup/edu/poc/orderservice/controller/OrderServiceController.java
+++ src/main/java/swrup/edu/poc/orderservice/controller/OrderServiceController.java
@@ -20,7 +20,9 @@ public class OrderServiceController {
 
     @GetMapping
     public List<Order> fetchOrders() {
-        return orderDao.getOrders().stream().
-                sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+       /* return orderDao.getOrders().stream().
+                sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());*/
+
+        return orderDao.getOrders();
     }
 }
